8168c960b74b25e65183e7ab2bde5646f60bb3d7
Batuhan BARAN <fb_batuhan_baran@hotmail.com>
2020-07-27 21:13:37 +0300

    Initial Commit

---
+++ TestWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestWeatherApp
+//
+//  Created by Batuhan Baran on 27.07.2020.
+//  Copyright © 2020 Batuhan Baran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
